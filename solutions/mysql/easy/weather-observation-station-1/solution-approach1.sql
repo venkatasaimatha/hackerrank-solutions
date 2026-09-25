b5a52c2709c39df1ddc627055ede2e08379de7b2
Venkata Matha <venkatasaimatha@gmail.com>
2026-09-24 20:44:12 -0500

    ✅ Weather Observation Station 1 [mysql] — approach1

---
+++ solutions/mysql/easy/weather-observation-station-1/solution-approach1.sql
@@ -0,0 +1,13 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
+-- Problem     Weather Observation Station 1
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-24, 08:44 p.m.
+-- ──────────────────────────────────────────────────
+
+Select CITY,STATE
+From STATION;
