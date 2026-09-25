48188c36b6ae8defd9861766dad23281272e4982
Venkata Matha <venkatasaimatha@gmail.com>
2026-09-24 20:41:56 -0500

    ✅ Select By ID [mysql] — approach1

---
+++ solutions/mysql/easy/select-by-id/solution-approach1.sql
@@ -0,0 +1,14 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+-- Problem     Select By ID
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-24, 08:41 p.m.
+-- ──────────────────────────────────────────────────
+
+Select *
+From CITY
+Where ID=1661;
