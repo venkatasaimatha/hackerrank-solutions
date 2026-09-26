ffc2261ee3a185bcf3bff9488e09425dc3f5fa43
Venkata Matha <venkatasaimatha@gmail.com>
2026-09-26 02:10:32 -0500

    ✅ Employee Names [mysql] — approach1

---
+++ solutions/mysql/easy/name-of-employees/solution-approach1.sql
@@ -0,0 +1,14 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+-- Problem     Employee Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-26, 02:10 a.m.
+-- ──────────────────────────────────────────────────
+
+Select name
+From Employee
+Order By name ASC;
